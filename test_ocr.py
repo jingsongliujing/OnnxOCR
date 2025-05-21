0eb754ac6b378ab334a4f0b96f5a5ae4859b9993
jingsongliujing <2262720766@qq.com>
2025-05-21 12:58:55 +0800

    PPOCRv5模型更新

--- test_ocr.py
+++ test_ocr.py
@@ -2,13 +2,14 @@ import cv2
 import time
 from onnxocr.onnx_paddleocr import ONNXPaddleOcr,sav2Img
 import sys
+import time
 #固定到onnx路径·
 # sys.path.append('./paddle_to_onnx/onnx')
 
 model = ONNXPaddleOcr(use_angle_cls=True, use_gpu=False)
 
 
-img = cv2.imread('./onnxocr/test_images/1.jpg')
+img = cv2.imread('./onnxocr/test_images/715873facf064583b44ef28295126fa7.jpg')
 s = time.time()
 result = model.ocr(img)
 e = time.time()
@@ -17,4 +18,4 @@ print("result:", result)
 for box in result[0]:
     print(box)
 
-sav2Img(img, result)
+sav2Img(img, result,name=str(time.time())+'.jpg')
